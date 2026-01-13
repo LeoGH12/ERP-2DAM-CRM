d6a783883634abbd8a5089b8239d07e9c3d2417d
Leo <leonardo_trabajo@outlook.es>
2026-01-13 13:22:52 +0100

    Add files via upload

---
+++ ERP2DAMCRM/src/main/java/edu/api/erp2damcrm/Erp2DamcrmApplication.java
@@ -0,0 +1,13 @@
+package edu.api.erp2damcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Erp2DamcrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Erp2DamcrmApplication.class, args);
+    }
+
+}
---
+++ ERP2DAMCRM/src/test/java/edu/api/erp2damcrm/Erp2DamcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.api.erp2damcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Erp2DamcrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
