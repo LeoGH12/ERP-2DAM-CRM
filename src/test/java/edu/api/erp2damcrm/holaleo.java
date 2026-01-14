112d227ae46578f98d1d62f3a164c896617f11b0
LeoGH12 <leonardo_educacion@outlook.es>
2026-01-14 08:49:29 +0100

    Cambio Leo

--- src/test/java/edu/api/erp2damcrm/holaleo.java
+++ src/test/java/edu/api/erp2damcrm/holaleo.java
@@ -2,6 +2,6 @@ package edu.api.erp2damcrm;
 
 public class holaleo {
     public static void main args() {
-        System.out.println("Hola Leo");
+        System.out.println("Hola Leo 2");
     }
 }
